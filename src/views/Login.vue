29de31c7a4b644e8c3e3bbe0c066da92709b9340
jianshujie <yaoqiwood@gmail.com>
2023-11-04 17:00:55 +0800

    修改了登陆界面 增加了几个基础配置 api设定 以及 各种环境下的开发配置 axios 等

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="login-background">
     <el-card class="login-card">
-      <el-form ref="loginForm" label-position="right" :model="loginForm" class="login-form" label-width="70px">
-        <el-form-item label="用户名：">
-          <el-input v-model="loginForm.username"></el-input>
+      <el-form ref="loginForm"
+               label-position="right"
+               :model="loginForm"
+               class="login-form"
+               label-width="70px"
+               :rules="loginRules">
+        <el-form-item label="用户名："
+                      prop="username">
+          <el-input
+            v-model="loginForm.username"
+            placeholder="请输入用户名"
+          ></el-input>
         </el-form-item>
         <el-form-item label="密码：">
-          <el-input type="password" v-model="loginForm.password"></el-input>
+          <el-input type="password"
+                    placeholder="请输入密码"
+                    v-model="loginForm.password"></el-input>
         </el-form-item>
-        <el-form-item v-if="showCaptcha" label="验证码：">
-          <el-input v-model="loginForm.captcha"></el-input>
+        <el-form-item v-if="showCaptcha"
+                      label="验证码：">
+          <el-row gutter="20">
+            <el-col :span="17"> <!-- 增加 input 框的宽度 -->
+              <el-input v-model="loginForm.captcha"
+                        placeholder="请输入验证码"
+                        style="width: 100%;"></el-input> <!-- 设置宽度为 100% -->
+            </el-col>
+            <el-col :span="7"> <!-- 减少图片的宽度 -->
+              <img :src="captChaUrl"
+                   style="height: 35px; cursor: pointer; border-radius: 10px"
+                   @click="refreshCaptcha"
+                   alt=""/>
+            </el-col>
+          </el-row>
         </el-form-item>
-        <el-button type="primary" style="width: 80%; left: -50% ;transform: translateX(13%)" @click="handleSubmit">登录
+
+
+        <el-button type="primary"
+                   style="width: 80%; left: -50% ;transform: translateX(13%)"
+                   @click="handleSubmit">登录
         </el-button>
       </el-form>
     </el-card>
@@ -30,6 +58,8 @@ export default {
       },
       loginAttempts: 0,
       showCaptcha: false,
+      // showCaptcha: true,
+      captChaUrl: `${process.env.VUE_APP_BASE_API_URL}/getCaptcha`
     };
   },
   methods: {
@@ -38,6 +68,9 @@ export default {
       // 如果登录失败，增加 loginAttempts
       // 如果 loginAttempts >= 3，显示验证码
     },
+    refreshCaptcha() {
+      this.captChaUrl = `${process.env.VUE_APP_BASE_API_URL}/getCaptcha?${new Date().getTime()}`
+    }
   },
   watch: {
     loginAttempts(val) {
