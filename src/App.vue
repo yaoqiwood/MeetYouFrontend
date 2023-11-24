e7237fa2e9e55f0fadb66175c8180111014f1071
jianshujie <yaoqiwood@gmail.com>
2023-11-24 13:17:34 +0800

    *前端内容打通

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+<style>
+body {
+  margin: 0 !important;
+  overflow: hidden;
+}
+</style>
+
+
